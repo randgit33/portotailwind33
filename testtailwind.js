3bc328106c65420cab1d1c691714b5ecb8669303
randgit33 <randeygiffary33@gmail.com>
2025-03-11 16:26:21 +0800

    test commit

---
+++ testtailwind.js
@@ -0,0 +1,11 @@
+document.getElementById("openIG").addEventListener("click", function(){
+    window.open("https://instagram.com/randfy_");
+});
+
+document.getElementById("openLinkd").addEventListener("click", function(){
+    window.open("https://www.linkedin.com/in/randgfry/");
+});
+
+document.getElementById("openGIT").addEventListener("click", function(){
+    window.open("https://github.com/randgit33");
+});
